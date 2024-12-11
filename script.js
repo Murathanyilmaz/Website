b6c8016668ba5818189620722790b96818876ed5
Murathan <97229162+Murathanyilmaz@users.noreply.github.com>
2024-12-11 18:36:17 +0000

    Puzzle segment added

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const jumbleButton = document.getElementById("jumbleButton");
 const diceButton = document.getElementById("diceButton");
 const guessButton = document.getElementById("guessButton");
 const memoryGameButton = document.getElementById("memoryGameButton");
+const gridPuzzleButton = document.getElementById("gridPuzzleButton");
 
 portfolioButton.addEventListener("click", () => {
     document.querySelector(".portfolio").classList.remove("hidden");
@@ -16,6 +17,7 @@ portfolioButton.addEventListener("click", () => {
     document.querySelector(".pig-game-desc").classList.add("hidden");
     document.querySelector(".guess-game").classList.add("hidden");
     document.querySelector(".memorygame").classList.add("hidden");
+    document.querySelector(".gridpuzzle").classList.add("hidden");
 })
 lensesButton.addEventListener("click", () => {
     document.querySelector(".portfolio").classList.add("hidden");
@@ -26,6 +28,7 @@ lensesButton.addEventListener("click", () => {
     document.querySelector(".pig-game-desc").classList.add("hidden");
     document.querySelector(".guess-game").classList.add("hidden");
     document.querySelector(".memorygame").classList.add("hidden");
+    document.querySelector(".gridpuzzle").classList.add("hidden");
 })
 unityButton.addEventListener("click", () => {
     document.querySelector(".portfolio").classList.add("hidden");
@@ -36,6 +39,7 @@ unityButton.addEventListener("click", () => {
     document.querySelector(".pig-game-desc").classList.add("hidden");
     document.querySelector(".guess-game").classList.add("hidden");
     document.querySelector(".memorygame").classList.add("hidden");
+    document.querySelector(".gridpuzzle").classList.add("hidden");
 })
 jumbleButton.addEventListener("click", () => {
     document.querySelector(".portfolio").classList.add("hidden");
@@ -46,6 +50,7 @@ jumbleButton.addEventListener("click", () => {
     document.querySelector(".pig-game-desc").classList.add("hidden");
     document.querySelector(".guess-game").classList.add("hidden");
     document.querySelector(".memorygame").classList.add("hidden");
+    document.querySelector(".gridpuzzle").classList.add("hidden");
 })
 diceButton.addEventListener("click", () => {
     document.querySelector(".portfolio").classList.add("hidden");
@@ -56,6 +61,7 @@ diceButton.addEventListener("click", () => {
     document.querySelector(".pig-game-desc").classList.remove("hidden");
     document.querySelector(".guess-game").classList.add("hidden");
     document.querySelector(".memorygame").classList.add("hidden");
+    document.querySelector(".gridpuzzle").classList.add("hidden");
 })
 guessButton.addEventListener("click", () => {
     document.querySelector(".portfolio").classList.add("hidden");
@@ -66,6 +72,7 @@ guessButton.addEventListener("click", () => {
     document.querySelector(".pig-game-desc").classList.add("hidden");
     document.querySelector(".guess-game").classList.remove("hidden");
     document.querySelector(".memorygame").classList.add("hidden");
+    document.querySelector(".gridpuzzle").classList.add("hidden");
 })
 memoryGameButton.addEventListener("click", () => {
     document.querySelector(".portfolio").classList.add("hidden");
@@ -76,8 +83,19 @@ memoryGameButton.addEventListener("click", () => {
     document.querySelector(".pig-game-desc").classList.add("hidden");
     document.querySelector(".guess-game").classList.add("hidden");
     document.querySelector(".memorygame").classList.remove("hidden");
+    document.querySelector(".gridpuzzle").classList.add("hidden");
 })
-
+gridPuzzleButton.addEventListener("click", () => {
+    document.querySelector(".portfolio").classList.add("hidden");
+    document.querySelector(".lenses").classList.add("hidden");
+    document.querySelector(".unity").classList.add("hidden");
+    document.querySelector(".jumble-game").classList.add("hidden");
+    document.querySelector(".pig-game").classList.add("hidden");
+    document.querySelector(".pig-game-desc").classList.add("hidden");
+    document.querySelector(".guess-game").classList.add("hidden");
+    document.querySelector(".memorygame").classList.add("hidden");
+    document.querySelector(".gridpuzzle").classList.remove("hidden");
+})
 function dropDownFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
     document.getElementById("arrow").classList.toggle("fa-caret-down");
